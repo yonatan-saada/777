bd342350df70efb8f98d3d0f7991032ee5d28ffb
yehonatan saada <yonatan156148@gmail.com>
2025-06-22 18:12:54 +0300

    new

--- 158.py
+++ 158.py
@@ -1,8 +1,12 @@
-o = [[1,2,3],
-     [4,5,6],
-     [7,8,9]]
-g = [[1,2],
-     [4,5]]
-for i in range(1,11):
-     for g in range(1,11):
-          print(i*g)
+# o = [[1,2,3],
+#      [4,5,6],
+#      [7,8,9]]
+# g = [[1,2],
+#      [4,5]]
+# for i in range(1,11):
+#      for g in range(1,11):
+#           print(i*g)
+for i in range(1,6):
+     for g in range(1,6):
+          print(i*g,end="\t")
+     print()
